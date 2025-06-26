1aff9f1c234b1ea5375cba5d64c3f54c13991fb0
Ayush Mehta <ayushmehtabps@gmail.com>
2025-06-26 19:35:27 +0530

    add

--- app/admin/createblog/action.ts
+++ app/admin/createblog/action.ts
@@ -1,19 +1,19 @@
 "use server "
-import prisma from "@/lib/db";
+import { prisma } from "@/app/utilis/db";
 import { redirect } from "next/navigation"
 
 export default async function handleSubmission(formData:FormData) {
   const title = formData.get("title") as string | null;
   const content = formData.get("content") as string | null;
   const url = formData.get("url") as string | null;
+     if (!title || !content || !url) {
+    throw new Error("Missing form fields");
+  }
 
-
-
-  await prisma.BlogPost.create({
+await prisma.blogPost.create({
     data:{
         title,
         content,
-        imageUrl
+        imageUrl:url,
     }
-  })
-}
+})
