e69357c7a5b6c0a7053e3b1ef0f8dddb1537a2b4
Ayush Mehta <ayushmehtabps@gmail.com>
2025-07-11 14:40:40 +0530

    add

--- app/admin/createblog/page.tsx
+++ app/admin/createblog/page.tsx
@@ -10,7 +10,6 @@ import {
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { Textarea } from "@/components/ui/textarea";
 import SubmitButton from "@/components/general/SumbitButton";
 import handleSubmission from "./action";
 
