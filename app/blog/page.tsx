9758fe8606b458c9ba3ecef6d82e14b2159ec87b
Ayush Mehta <ayushmehtabps@gmail.com>
2025-07-30 11:15:29 +0530

    final check

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,4 +1,4 @@
-export const revalidate = 60; 
+export const dynamic = "force-dynamic"; 
 import BlogCard from "@/components/general/BlogCrad";
 import { prisma } from "../utilis/db";
 import { Image } from "lucide-react";
